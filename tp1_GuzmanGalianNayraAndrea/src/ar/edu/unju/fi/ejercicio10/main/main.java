ae8fba525d7f13dc4e5c4c052d056959f081fc93
Usuario <Usuario@DESKTOP-43M4CCK>
2024-04-16 00:04:07 -0300

    Paquetes y clases del ej10

---
+++ tp1_GuzmanGalianNayraAndrea/src/ar/edu/unju/fi/ejercicio10/main/main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio10.main;
+import ar.edu.unju.fi.ejercicio10.model.pizza;
+public class main {
+
+	public static void main(String[] args) {			
+			menu unMenu = new menu();	
+			pizza pizza = new pizza();
+			unMenu.generarMenu(pizza);		
+					
+		}
+	}
+
+
